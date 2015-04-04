e014da230a0a155d55f48c68e238eac7dbd5cd0a
mihai9323 <mihai9323@gmail.com>
2015-04-04 15:28:20 +0200

    shop

--- Assets/Logic/Shop.cs
+++ Assets/Logic/Shop.cs
@@ -10,47 +10,78 @@ public class Shop : MonoBehaviour {
 	[SerializeField] Text T_other_speed;
 	[SerializeField] Text T_other_shine;
 	
-	[SerializeField] Text BuyPrice;
-	[SerializeField] Text SwapPrice;
+	[SerializeField] Text T_Buy;
+	[SerializeField] Text T_Swap;
+	[SerializeField] Text T_Sell;
+	
+	[SerializeField] Button buyButton;
+	[SerializeField] Button swapButton;
+	[SerializeField] Button sellButton;
 	
 	private float myItemValue, otherItemValue;
-	public Item sellItem, buyItem;
-	public void CompareMyItem(Item item){
-		sellItem = item;
+	
+	private Item _buyItem,_sellItem;
+	
+	public Item sellItem{
+		set{
+			_sellItem = value;
+			DisplayButtons();
+			
+			}
+		get{
+			if(_sellItem == null) _sellItem = new Item();
+			return _sellItem;
+		    }
+		}
+	public Item buyItem{
+		set{
+			_buyItem = value;
+			DisplayButtons();
+			
+		}
+		get{
+			if(_buyItem == null) _buyItem = new Item();
+			return _buyItem;
+		}
+	}
+	private void Start(){
+		buyButton.gameObject.SetActive(false);
+		sellButton.gameObject.SetActive(false);
+		swapButton.gameObject.SetActive(false);
+	}
+public void CompareMyItem(Item item){
+		
 		if(item.itemType == Item.ItemType.None){
 			T_my_dmg.text = "DMG: ";
 			T_my_speed.text = "SPEED: ";
 			T_my_shine.text = "SHINE: ";
 			myItemValue = 0;
-			BuyPrice.text = "Buy for "+otherItemValue;
-			SwapPrice.text = "Swap for "+(otherItemValue- myItemValue).ToString();
+			
 		}
 		else if(item.itemType == Item.ItemType.Armor){
 			T_my_dmg.text = "DEF: "+item.Defence;
 			T_my_speed.text = "";
 			T_my_shine.text= "SHINE: "+ item.shine;
 			myItemValue = item.Value;
-			BuyPrice.text = "Buy for "+otherItemValue;
-			SwapPrice.text = "Swap for "+(otherItemValue- myItemValue).ToString();
+			
 			
 		}else{
 			T_my_dmg.text = "DMG: "+item.Damage;
 			T_my_speed.text = "SPD: "+item.Speed;
 			T_my_shine.text = "SHINE: "+ item.shine;
 			myItemValue = item.Value;
-			BuyPrice.text = "Buy for "+otherItemValue;
-			SwapPrice.text = "Swap for "+(otherItemValue- myItemValue).ToString();
+			
 		}
+		sellItem = item;
 	}
 	public void CompareSaleItem(Item item){
-		buyItem = item;
+		
 		if(item.itemType == Item.ItemType.None){
 			T_other_dmg.text = "DMG: ";
 			T_other_speed.text = "SPEED: ";
 			T_other_shine.text = "SHINE: ";
 			otherItemValue = 0;
-			BuyPrice.text = "Buy for "+otherItemValue;
-			SwapPrice.text = "Swap for "+otherItemValue;
+			
 			
 		}
 		else if(item.itemType == Item.ItemType.Armor){
@@ -58,17 +89,54 @@ public class Shop : MonoBehaviour {
 			T_other_speed.text = "";
 			T_other_shine.text = "SHINE: "+ item.shine;
 			otherItemValue = item.Value;
-			BuyPrice.text = "Buy for "+otherItemValue;
-			SwapPrice.text = "Swap for "+(otherItemValue- myItemValue).ToString();
+		
 			
 		}else{
 			T_other_dmg.text = "DMG: "+item.Damage;
 			T_other_speed.text = "SPD: "+item.Speed;
 			T_other_shine.text = "SHINE: "+ item.shine;
 			otherItemValue = item.Value;
-			BuyPrice.text = "Buy for "+otherItemValue;
-			SwapPrice.text = "Swap for "+(otherItemValue- myItemValue).ToString();
+			
 		}
+		buyItem = item;
+	}
+	private void DisplayButtons(){
+		if(buyItem.itemType == Item.ItemType.None && sellItem.itemType == Item.ItemType.None){
+			//we have no items selected
+			buyButton.gameObject.SetActive(false);
+			swapButton.gameObject.SetActive(false);
+			sellButton.gameObject.SetActive(false);
+		}else if(buyItem.itemType != Item.ItemType.None && sellItem.itemType != Item.ItemType.None){
+			//we have both items
+			buyButton.gameObject.SetActive(false);
+			swapButton.gameObject.SetActive(true);
+			sellButton.gameObject.SetActive(false);
+			
+			T_Swap.text = "Swap For: "+ Mathf.Max (10,otherItemValue - myItemValue);
+		
+		}else if(buyItem.itemType != Item.ItemType.None && sellItem.itemType == Item.ItemType.None){
+			// we only have the shop item
+			buyButton.gameObject.SetActive(true);
+			swapButton.gameObject.SetActive(false);
+			sellButton.gameObject.SetActive(false);
+			
+			T_Buy.text = "Buy For: "+ Mathf.Max (10,otherItemValue);
+			
+			
+		}else if(buyItem.itemType == Item.ItemType.None && sellItem.itemType != Item.ItemType.None){
+			
+			//only our item selected
+			buyButton.gameObject.SetActive(false);
+			swapButton.gameObject.SetActive(false);
+			sellButton.gameObject.SetActive(true);
+			
+			T_Sell.text = "Sell For: "+ Mathf.Max (10,myItemValue);
+			
+			
+		}
+		
+		
+		
 	}
 	public void ShowShop(){
 		HubManager.ShowShop();
