1c869aa65ee5f2e91ef7c5f15efa7f24d233b2e4
mihai9323 <mihai9323@gmail.com>
2015-06-14 19:37:19 +0200

    fixed compiler errors

--- Assets/Logic/DramaManager/DramaData.cs
+++ Assets/Logic/DramaManager/DramaData.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+#if ADD_DATA
 using UnityEditor;
-
+#endif
 
 namespace DramaPack{
 #if ADD_DATA 
@@ -11,15 +12,15 @@ namespace DramaPack{
 
 		public string name;
 	
-		#if UNITY_EDITOR
+		#if ADD_DATA
 		protected virtual void Update () {
 			if (string.IsNullOrEmpty (name)) {
 				name = this.GetType().ToString();
 			}
 			this.gameObject.name = this.name;
 		}
-		#elif
-		protected override void Update(){
+		#else
+		protected virtual void Update(){
 			
 		}
 		#endif
