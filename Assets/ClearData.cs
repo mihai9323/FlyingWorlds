ae6e3815f258488133363f86502eff52c6f50880
mihai9323 <mihai9323@gmail.com>
2015-06-14 16:50:44 +0200

    clear data on startup

---
+++ Assets/ClearData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClearData : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PersistentData.ClearData ();
+	}
+
+}
